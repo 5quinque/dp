6a325533ceb6d6387ec12d8e03c3f394e71f0cbe
Ryan Linnit <ryan@ryanl.co.uk>
2021-09-04 08:40:31 +0100

    read stream from local fs flysystem

--- src/MessageHandler/FileMessageHandler.php
+++ src/MessageHandler/FileMessageHandler.php
@@ -6,45 +6,44 @@ use App\Message\FileMessage;
 use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use League\Flysystem\FilesystemInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class FileMessageHandler implements MessageHandlerInterface
 {
     private $mediaRepository;
+    private $localFilesystem;
     private $mediaFilesystem;
-    // private $om;
     private $entityManager;
-    protected $parameterBag;
 
     public function __construct(
         MediaRepository $mediaRepository,
         EntityManagerInterface $entityManager,
-        FilesystemInterface $mediaFilesystem,
-        ParameterBagInterface $parameterBag
-        // EntityManager $em
+        FilesystemInterface $localFilesystem,
+        FilesystemInterface $mediaFilesystem
     ) {
         $this->mediaRepository = $mediaRepository;
         $this->mediaFilesystem = $mediaFilesystem;
+        $this->localFilesystem = $localFilesystem;
         $this->entityManager = $entityManager;
-        $this->parameterBag = $parameterBag;
     }
 
     public function __invoke(FileMessage $fileMessage)
     {
         $file = $this->mediaRepository->find($fileMessage->getFileId());
 
-        $projectDir = $this->parameterBag->get('kernel.project_dir');
-
-        $this->mediaFilesystem->writeStream(
+        $remoteTransferSuccess = $this->mediaFilesystem->writeStream(
             $file->getFilename(),
-            fopen("{$projectDir}/var/storage/default/{$file->getFilename()}", "r")
+            $this->localFilesystem->readStream($file->getFilename())
         );
 
+        if ($remoteTransferSuccess) {
+            $this->localFilesystem->delete($file->getFilename());
 
-        $file->setFilesystem("media");
-
-        $this->entityManager->persist($file);
-        $this->entityManager->flush();
+            $file->setFilesystem("media");
+            $this->entityManager->persist($file);
+            $this->entityManager->flush();
+        } else {
+            // [TODO]: Handle remote upload failure..
+        }
     }
 }
